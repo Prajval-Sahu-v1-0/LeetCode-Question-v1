962865ec5aa94279f8c18b84ff717d02d69c1ffc
Prajval-Sahu <97943207+Prajval-Sahu-v1-0@users.noreply.github.com>
2026-02-07 22:39:28 +0530

    Time: 3 ms (99.65%), Space: 94.5 MB (55.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-      /*  ListNode slow = head, fast = head;
+        ListNode slow = head, fast = head;
         while(fast != null && fast.next != null)
         {
             slow = slow.next;
@@ -31,17 +31,6 @@ class Solution {
             if(prev.val != head.val)return false;
             prev = prev.next;
             head = head.next;
-        }return true; */
-
-        StringBuilder sb = new StringBuilder();
-        
-        ListNode temp = head;
-        while(temp != null){
-            sb.append(temp.val);
-            temp= temp.next;
-        }
-        String orignal = sb.toString();
-        String reversed = sb.reverse().toString();
-        return orignal.equals(reversed);
+        }return true;
     }
 }
